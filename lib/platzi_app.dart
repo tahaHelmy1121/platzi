668248134c1fc811360f3fe8b4ea25a6a97c3d21
tahaHelmy1121 <tahahelmy196@gmail.com>
2024-04-27 20:10:13 +0300

    addFirebaseServAndEnv

---
+++ lib/platzi_app.dart
@@ -0,0 +1,21 @@
+  import 'package:flutter/material.dart';
+
+         class AppPlatzi extends StatelessWidget {
+           const AppPlatzi({super.key});
+
+           @override
+           Widget build(BuildContext context) {
+             return MaterialApp(
+               home: LoginScreen(),
+             );
+           }
+         }
+         class LoginScreen extends StatelessWidget {
+           const LoginScreen({super.key});
+         
+           @override
+           Widget build(BuildContext context) {
+             return const Placeholder();
+           }
+         }
+         
